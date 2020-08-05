b8bdda2c1ea7a9d1cdb84cd81110179a5207d55f
marcochanhk <marco95968@ymail.com>
2020-08-05 16:43:20 +0800

    Exercise 3-1 Delivery List Map in tab

--- force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
+++ force-app/Exercises/main/default/lwc/studentBrowser/studentBrowser.js
@@ -1,33 +1,48 @@
 import { LightningElement, track, wire } from 'lwc';
 import getStudents from '@salesforce/apex/StudentBrowser.getStudents';
+import {publish, MessageContext} from 'lightning/messageService';
+import SELECTED_STUDENT_CHANNEL from '@salesforce/messageChannel/SelectedStudentChannel__c';
 
 export default class StudentBrowser extends LightningElement {
     // @track studentList = [];
+
+    // constructor() {
+    //     super();
+    //     // Call the super constructor here...
+    //     const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul', "Amit", "Simon"];
+    //     studentNames.forEach((studentName, index) => {
+    //       this.studentList.push(
+    //         {
+    //           'sobjectType' : 'Contact',
+    //           'Name' : studentName,
+    //           'PhotoUrl': '/services/images/photo/003B0FakePictId',
+    //           'Id' : index
+    //         }
+    //       );
+    //     })
+    //   }
+      
+    @wire(getStudents, {instructorId: '$selectedInstructorId', courseDeliveryId: '$selectedDeliveryId'}) students;
     selectedDeliveryId = '';
     selectedInstructorId = '';
-    //@wire(getStudents,{instructorId:"", courseDeliveryId:""}) students;
 
-    @wire(getStudents, { instructorId: '$selectedInstructorId', courseDeliveryId: '$selectedDeliveryId' })students; //2-9
-    
     handleFilterChange(event){
         this.selectedDeliveryId = event.detail.deliveryId;
         this.selectedInstructorId = event.detail.instructorId;
     }
 
+    @wire(MessageContext) messageContext;
 
-    // constructor() {
-    //     super();
-    //     const studentNames = ['Rad', 'Stuart', 'Andres', 'Rahul',
-    //         "Amit", "Simon"];
-    //     studentNames.forEach((studentName, index) => {
-    //         this.studentList.push(
-    //             {
-    //                 'sobjectType': 'Contact',
-    //                 'Name': studentName,
-    //                 'PhotoUrl': '/services/images/photo/003B0FakePictId',
-    //                 'Id': index
-    //             }
-    //         );
-    //     })
-    // }
+    handleStudentSelected(event){
+        const studentId = event.detail.studentId;
+        this.updateSelectedStudent(studentId);
+    }
+
+    updateSelectedStudent(studentId){
+        const message = {
+            studentId: studentId
+        }
+        publish(this.messageContext, SELECTED_STUDENT_CHANNEL, message);
+    }
+    
 }
--- force-app/Exercises/main/default/lwc/studentTile/studentTile.js
+++ force-app/Exercises/main/default/lwc/studentTile/studentTile.js
@@ -1,22 +1,28 @@
 import { LightningElement, api } from 'lwc';
 
 export default class StudentTile extends LightningElement {
-    @api student = { Name: 'Marco', PhotoUrl: '/services/images/photo/003B0FakePicId' };
-    @api selected = false;
     @api selectedStudentId = '';
+    @api student = {
+        Name: 'Peter Chan',
+        PhotoUrl: '/services/images/photo/003B0FakePictId',
+    };
+    // @api selected = false;
 
-    get tileSelected() {
+    get tileSelected(){
+        // return this.selected ? 'tile selected': 'tile'; 
         return (this.selectedStudentId===this.student.Id) ? "tile selected" : "tile";
     }
 
-    studentClick() {
-        // alert(this.student.Name)
-        // debugger;
-        // console.log("Name", this.student.Name);
-        // console.log("Photo", this.student.PhotoUrl)
+    studentClick(){
+        // alert(this.student.Name);
         const evt = new CustomEvent('studentselected', {
-            detail: { studentId: this.student.Id }
+            bubbles: true, composed: true,
+            detail: {
+                studentId: this.student.Id
+            }
         });
         this.dispatchEvent(evt);
     }
+
+    
 }
